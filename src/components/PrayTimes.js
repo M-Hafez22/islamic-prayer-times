55908aab4a13c5df3a1fcc114b6a1deff318e177
Mohamed Hafez <mohamedhafezmansour@gmail.com>
2020-04-12 06:01:51 +0200

    Add comments

--- src/components/PrayTimes.js
+++ src/components/PrayTimes.js
@@ -4,14 +4,16 @@ import HijriDate from './HijriDate'
 import PrayCard from './PrayCard'
 
 export default function PrayTimes() {
-
+    // Get Data
     const [loaded, data] = useFetch(`http://api.aladhan.com/v1/timings/12-4-2020?latitude=35.851709500000005&longitude=55.924600499999997&method=5`);
-    const hijri = loaded && data.date.hijri  ;
+
+    // Get Hijri Date
+    const hijri = loaded && data.date.hijri;
+
+    // Get Pray Time
     const timings = loaded && data.timings;
-    
     const PrayNames =  Object.keys(timings);
     const PrayTimes =  Object.values(timings);
-
     const prayTimeList = PrayTimes.map((p, i) => (<PrayCard  key={p} name={PrayNames[i]} time={p}/>))
 
     return (
@@ -19,7 +21,6 @@ export default function PrayTimes() {
             <h1>أوقات الصلاة</h1>
             {loaded &&  <HijriDate hijri={hijri}/>  }
             {loaded &&   prayTimeList}
-            
         </div>
     )
 }
