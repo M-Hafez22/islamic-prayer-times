6d8338df1b455ac788bb798f61f334d29572cad6
Mohamed Hafez <mohamedhafezmansour@gmail.com>
2020-04-12 04:52:15 +0200

    Display Hijri date

--- src/components/HijriDate.js
+++ src/components/HijriDate.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function HijriDate({hijri}) {
     return (
         <div>
-            <h2>{hijri.date}</h2>
+            <h2>{hijri.weekday.ar} {hijri.day} {hijri.month.ar} {hijri.year}</h2>
         </div>
     )
 }
