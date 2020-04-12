792057c5a3bc9946206407d7c41be5d5c1a133d5
Mohamed Hafez <mohamedhafezmansour@gmail.com>
2020-04-12 06:47:45 +0200

    Get the user loaction

--- src/components/PrayTimes.js
+++ src/components/PrayTimes.js
@@ -2,19 +2,24 @@ import React from 'react'
 import {useFetch} from './useFetch'
 import HijriDate from './HijriDate'
 import PrayCard from './PrayCard'
+import {useLoaction} from './useLocation';
 
 export default function PrayTimes() {
+
+    // Get Local coords
+    const [latitude, longitude] = useLoaction();
     // Get Data
-    const [loaded, data] = useFetch(`http://api.aladhan.com/v1/timings/12-4-2020?latitude=35.851709500000005&longitude=55.924600499999997&method=5`);
+    const [loaded, data] = useFetch(`http://api.aladhan.com/v1/timings/12-4-2020?latitude=${latitude}&longitude=${longitude}&method=5`);
 
     // Get Hijri Date
     const hijri = loaded && data.date.hijri;
 
     // Get Pray Time
     const timings = loaded && data.timings;
-    const PrayNames =  Object.keys(timings);
-    const PrayTimes =  Object.values(timings);
-    const prayTimeList = PrayTimes.map((p, i) => (<PrayCard  key={p} name={PrayNames[i]} time={p}/>))
+    const prayNames =  Object.keys(timings);
+    console.log(prayNames);
+    const prayTimes =  Object.values(timings);
+    const prayTimeList = prayNames.map((p, i) =>  (<PrayCard  key={p} name={p} time={prayTimes[i]}/>))
 
     return (
         <div>
