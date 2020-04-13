f57a703b10fed15ff74f4936d497bf3fd33fe901
Mohamed Hafez <mohamedhafezmansour@gmail.com>
2020-04-13 05:51:55 +0200

    Display only prayers

--- src/components/PrayTimes.js
+++ src/components/PrayTimes.js
@@ -11,10 +11,11 @@ function PrayTimes() {
     const [loaded, data] = useContext(FetchedDataContext);
     // Get Pray Time
     const timings = loaded && data.timings;
+    const prayers = ['Fajr', 'Dhuhr', 'Asr', 'Maghrib', 'Isha'];
     const prayNames =  Object.keys(timings);
     const prayTimes =  Object.values(timings);
     const prayTimeList = prayNames.map((p, i) =>  (
-        <PrayCard  key={p} name={p} time={prayTimes[i]}/>
+        prayers.includes(p) && <PrayCard  key={p} name={p} time={prayTimes[i]}/>
     ))
 
     return (
