a9c00e390f0f260afed2ed3c4dc66b4a0084b3a9
Mohamed Hafez <mohamedhafezmansour@gmail.com>
2020-05-10 05:13:56 +0200

    Adjustes hijri date

--- src/components/contexts/FetchedDataContext.js
+++ src/components/contexts/FetchedDataContext.js
@@ -1,22 +1,22 @@
-import React, {createContext}from "react";
-import {useFetch} from '../hooks/useFetch'
-import {useLoaction} from '../hooks/useLocation';
+import React, { createContext } from "react";
+import { useFetch } from '../hooks/useFetch'
+import { useLoaction } from '../hooks/useLocation';
 
 export const FetchedDataContext = createContext();
 
 export function FetchedDataProvider(props) {
-    
+
     // Get Local coords
     const [latitude, longitude] = useLoaction();
     // Get time
     const date = Math.floor(Date.now() / 1000);
     // Get Data
-    const [loaded, data] = useFetch(`http://api.aladhan.com/v1/timings/${date}?latitude=${latitude}&longitude=${longitude}&method=5&adjustment=1`);
+    const [loaded, data] = useFetch(`http://api.aladhan.com/v1/timings/${date}?latitude=${latitude}&longitude=${longitude}&method=5`);
 
-    return(
-    <FetchedDataContext.Provider value={[loaded, data]}>
-        {props.children}
-    </FetchedDataContext.Provider>
+    return (
+        <FetchedDataContext.Provider value={[loaded, data]}>
+            {props.children}
+        </FetchedDataContext.Provider>
     )
 }
 
