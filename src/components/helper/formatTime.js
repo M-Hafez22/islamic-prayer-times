af1ccefe22f8af42c3baee6636a63fe672fa7451
Mohamed Hafez <mohamedhafezmansour@gmail.com>
2020-06-27 02:58:04 +0200

    Fix Error
    handle input like 07:00

--- src/components/helper/formatTime.js
+++ src/components/helper/formatTime.js
@@ -1,12 +1,19 @@
+
 export const addZero = (number) => (number < 10 ? `0${number}` : number);
 
 export const to12Format = (time) => {
-  // get the hour
+
   let hour = time.slice(0,time.indexOf(":"));
+  let minute = time.slice(time.indexOf(":")+1);
+
   //convert to 12 Formate
   hour = (hour % 12) || 12;
-  // Add zero for
-  hour = addZero(hour);
 
-  return `${hour}${time.slice(time.indexOf(":"))}`;
+  // formate Look
+  hour = addZero(hour);
+  minute.length < 2
+    ? minute = addZero(minute)
+    : minute = minute;
+
+  return `${hour}:${minute}`;
 }
