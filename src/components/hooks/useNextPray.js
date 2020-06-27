124143f1e4af3c8626535c9cd4c23787b5112a05
Mohamed Hafez <mohamedhafezmansour@gmail.com>
2020-06-27 02:57:20 +0200

    Make the code more readable

--- src/components/hooks/useNextPray.js
+++ src/components/hooks/useNextPray.js
@@ -1,11 +1,14 @@
 
 import { useCurrentDate } from './useCurrentDate'
+import {addZero} from '../helper/formatTime'
+
 export default function useNextPrayer(arr) {
     // Get current time
     const date = useCurrentDate();
     // Get the date of the prayer
     const getTime = time => new Date(date.getFullYear(), date.getMonth(), date.getDate(), time.substring(0, 2), time.substring(3, 5), 0, 0);
-    const prayDate = arr.map(p => getTime(p))  
+    const prayDate = arr.map(p => getTime(p))
+    
     // Get the remaining time for each prayer
     const timeLeft = prayDate.map(p => p =(date - p))
    // time format
@@ -15,19 +18,20 @@ export default function useNextPrayer(arr) {
       num  %= 3600;
       const m = Math.floor(num / 60);
       const s =  num % 60;
-      return (`${h < 9 ? `0${h}` : h }:${m < 9 ? `0${m}` : m}:${s < 9 ? `0${s}` : s}`)
+      return (`${addZero(h)}:${addZero(m)}:${addZero(s)}`)
     }
-    // Get the remaining prayers for today 
+    // Get the remaining prayers for today
     const leftPray = timeLeft.filter(el => el < 0)
+
     // Get the remaining time for the next prayer
-    let remaineTime = leftPray.length > 0 ? ( Math.max(...leftPray)) : (86400000 - (date- prayDate[0]))
+    let remaineTime = leftPray.length > 0
+      ? ( Math.max(...leftPray))
+      : (86400000 - (date- prayDate[0]))
+
     // Get next Prayer index
-    const nextPrayer = timeLeft.indexOf(remaineTime) !== -1 ? timeLeft.indexOf(remaineTime) : 0
+    const nextPrayer = timeLeft.indexOf(remaineTime) !== -1
+      ? timeLeft.indexOf(remaineTime)
+      : 0
 
     return [getTimeFormat(remaineTime), nextPrayer]
   }
-
-
-
-
-
