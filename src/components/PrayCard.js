ce2d8a79ffb66055c492c96cc9c9ae3b7c937435
Mohamed Hafez <mohamedhafezmansour@gmail.com>
2021-10-04 23:51:09 +0200

    Documenting

--- src/components/PrayCard.js
+++ src/components/PrayCard.js
@@ -1,5 +1,13 @@
 import React from 'react'
 
+/**
+ * Display Pray Name and Time with next pray highlighted
+ * 
+ * @prop {string}  name the pray name
+ * @prop {string}  time  the pray time
+ * @prop {boolean}  active  is the next pray or not
+ * 
+ */
 export default function PrayCard({name, time, active}) {
 
     return (
