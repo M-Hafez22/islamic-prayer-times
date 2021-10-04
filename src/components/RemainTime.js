3dc028078c60f8c3b2dcc569317d1cf7928cd816
Mohamed Hafez <mohamedhafezmansour@gmail.com>
2021-10-04 23:44:11 +0200

    Documenting

--- src/components/RemainTime.js
+++ src/components/RemainTime.js
@@ -1,5 +1,15 @@
 import React from 'react'
 
+/**
+ * The next pray and Remaining Time for it
+ * 
+ * @prop {string} text a message
+ * @prop {string} prayer the next pray
+ * @prop {string} time The Remaining Time for the next pray
+ * 
+ * @returns 
+ */
+
 export default function RemainTime({text ,prayer, time}) {
 
     return (
