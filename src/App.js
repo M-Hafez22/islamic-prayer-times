2aff77d33886c15f17af2291c241e9c73bea330d
Mohamed Hafez <mohamedhafezmansour@gmail.com>
2022-01-10 22:35:12 +0200

    Adds dark CSS class to app

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
-import React from 'react';
+import {useContext} from 'react';
 import Clock from './components/Clock'
 import HijriDate from './components/HijriDate'
 import PrayTimes from './components/PrayTimes'
 import Footer from './components/Footer'
 import {FetchedDataProvider} from './components/contexts/FetchedDataContext'
 import {LanguageProvider} from './components/contexts/languageContext'
+import { ThemeContext } from './components/contexts/theme'
 import './styles/main.scss'
 
 function App() {
+  const [ {isDark} ] = useContext(ThemeContext);
+
   return (
-    <div className="App">
+    <div  className={isDark  ? 'app dark' : 'app light'}>
       <FetchedDataProvider>
         <LanguageProvider>
           <Clock />
