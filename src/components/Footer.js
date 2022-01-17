2bef4911284616bdad0689ce72d65c7a86435d2e
Mohamed Hafez <mohamedhafezmansour@gmail.com>
2022-01-18 00:56:20 +0200

    Improve color contrast

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,12 @@
-import React from 'react'
+import {useContext} from 'react'
 import LanguageToggle from './LanguageToggle'
 import TimeZone from './TimeZone'
+import { ThemeContext } from './contexts/theme'
 
 export default function Footer() {
+    const [{isDark} ] = useContext(ThemeContext);
     return (
-        <footer className="footer">
+        <footer className={isDark ? "footerDark" :"footerLight"}>
             <LanguageToggle />
             <TimeZone />
         </footer>
