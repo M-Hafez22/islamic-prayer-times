dca76b837dbc327e0979d0a5ea0986364c9f8c24
Mohamed Hafez <mohamedhafezmansour@gmail.com>
2022-01-18 23:01:16 +0200

    Style setting icon

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,17 +1,19 @@
-import {useContext, useState} from 'react'
-import Settings  from './footer/Settings';
+import { useContext, useState } from 'react'
+import Settings from './footer/Settings';
 import { ThemeContext } from './contexts/theme'
 
 export default function Footer() {
-    const [{isDark} ] = useContext(ThemeContext);
+    const [{ isDark }] = useContext(ThemeContext);
 
     // A state for Showing and Hiding Setting section
     const [show, setShow] = useState(false)
 
     return (
-        <footer className={isDark ? "footerDark" :"footerLight"}>
-            <button onClick={() => setShow(!show)}>⚙️</button>
-            <Settings show={show}/>
+        <footer className={isDark ? "footerDark" : "footerLight"}>
+            <button onClick={() => setShow(!show)} className='setting-btn'>
+                <img src="../assets/setting.png" alt="⚙️" />
+            </button>
+            <Settings show={show} />
         </footer>
     )
 }
