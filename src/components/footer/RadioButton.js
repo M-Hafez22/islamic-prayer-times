b16d811c314b6c5beb8dab4daa9d71d2b85b6ae9
Mohamed Hafez <mohamedhafezmansour@gmail.com>
2022-01-18 21:48:52 +0200

    Add dark class style

--- src/components/footer/RadioButton.js
+++ src/components/footer/RadioButton.js
@@ -1,8 +1,21 @@
-import React from 'react'
+import { useContext } from 'react'
+import { ThemeContext } from '../contexts/theme'
 
 function RadioButton({ label, value, onChange, checked }) {
+
+    // Toggle Themes (Dark, Light)
+    const [{ isDark }] = useContext(ThemeContext);
     return (
-        <input label={label} type="radio" id={label} onChange={onChange} name={label}  value={value}checked={checked} />
+        <input
+            label={label}
+            type="radio"
+            id={label}
+            onChange={onChange}
+            name={label}
+            value={value}
+            checked={checked}
+            className={isDark ? "dark" : "light"}
+        />
     )
 }
 
