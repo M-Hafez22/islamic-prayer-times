c01e9ee231fb010fbe9646cb64fff1d19d805649
Mohamed Hafez <mohamedhafezmansour@gmail.com>
2022-01-18 20:48:53 +0200

    Gather settings components

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,17 +1,12 @@
 import {useContext} from 'react'
-import LanguageToggle from './footer/LanguageToggle'
-import TimeZone from './footer/TimeZone'
-import ThemeToggle  from './footer/ThemeToggle';
+import Settings  from './footer/Settings';
 import { ThemeContext } from './contexts/theme'
 
 export default function Footer() {
     const [{isDark} ] = useContext(ThemeContext);
     return (
         <footer className={isDark ? "footerDark" :"footerLight"}>
-            <LanguageToggle />
-            <TimeZone />
-          <ThemeToggle />
-
+            <Settings />
         </footer>
     )
 }
---
+++ src/components/footer/Settings.js
@@ -0,0 +1,15 @@
+import LanguageToggle from './LanguageToggle'
+import TimeZone from './TimeZone'
+import ThemeToggle from './ThemeToggle';
+
+function Settings() {
+    return (
+        <div>
+            <LanguageToggle />
+            <TimeZone />
+            <ThemeToggle />
+        </div>
+    )
+}
+
+export default Settings
