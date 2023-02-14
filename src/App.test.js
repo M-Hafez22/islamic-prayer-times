1682366e81909cc634d202b63f4511ad05066f7f
Mohamed Hafez <mohamedhafezmansour@gmail.com>
2023-02-15 00:13:19 +0200

    ✅ Test Clock pass

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,21 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import App from './App';
+import { ThemeProvider } from './components/contexts/theme'
+import { to12Format, addLeadingZero } from './components/helper/formatTime'
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+import App from './App';
+describe('Render Clock', () => {
+  test('renders Hour:Minute in 12 format', () => {
+    const { getByText } = render(<ThemeProvider><App /></ThemeProvider>);
+    let date = new Date();
+    const currentTime = `${date.getHours()}:${date.getMinutes()}`;
+    const clock = getByText(to12Format(currentTime));
+    expect(clock).toBeInTheDocument();
+  });
+  it('Renders Second', () => {
+    const { getByText } = render(<ThemeProvider><App /></ThemeProvider>);
+    let date = new Date();
+    const seconds = getByText(`:${addLeadingZero(date.getSeconds())}`)
+    expect(seconds).toBeInTheDocument();
+  })
+})
