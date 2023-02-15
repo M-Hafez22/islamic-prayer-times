13057ea13b30a1141aa840044e4e7e33a64ecba9
Mohamed Hafez <mohamedhafezmansour@gmail.com>
2023-02-15 02:26:14 +0200

    ♻️ Refactor the test

--- src/App.test.js
+++ src/App.test.js
@@ -1,27 +1,27 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import { ThemeProvider } from './components/contexts/theme'
 import { to12Format, addLeadingZero } from './components/helper/formatTime'
 
 import App from './App';
 describe('Render Clock', () => {
+  let date = new Date();
+
+  beforeEach(() => {
+    render(<ThemeProvider><App /></ThemeProvider>);
+  })
   test('renders Hour:Minute in 12 format', () => {
-    const { getByText } = render(<ThemeProvider><App /></ThemeProvider>);
-    let date = new Date();
     const currentTime = `${date.getHours()}:${date.getMinutes()}`;
-    const clock = getByText(to12Format(currentTime));
-    expect(clock).toBeInTheDocument();
+    // const clock = getByText(to12Format(currentTime));
+    expect(screen.getByText(to12Format(currentTime))).toBeInTheDocument();
   });
   it('Renders Second', () => {
-    const { getByText } = render(<ThemeProvider><App /></ThemeProvider>);
     let date = new Date();
-    const seconds = getByText(`:${addLeadingZero(date.getSeconds())}`)
-    expect(seconds).toBeInTheDocument();
+    // const seconds = getByText(`:${addLeadingZero(date.getSeconds())}`)
+    expect(screen.getByText(`:${addLeadingZero(date.getSeconds())}`)).toBeInTheDocument();
   })
   it("render AM or PM", () => {
-    const { getByText } = render(<ThemeProvider><App /></ThemeProvider>);
-    let date = new Date();
-    const dayOrNight = getByText(date.getHours() < 12 ? "AM" : "PM")
-    expect(dayOrNight).toBeInTheDocument();
+    // const dayOrNight = getByText(date.getHours() < 12 ? "AM" : "PM")
+    expect(screen.getByText(date.getHours() < 12 ? "AM" : "PM")).toBeInTheDocument();
   })
 })
