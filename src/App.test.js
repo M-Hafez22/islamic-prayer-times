2bb3fe40511b6681b0c2d780e511aa43b8708f74
Mohamed Hafez <mohamedhafezmansour@gmail.com>
2023-02-15 02:46:47 +0200

    ♻️  Refactor the test

--- src/App.test.js
+++ src/App.test.js
@@ -1,37 +1,36 @@
-import React from 'react';
 import { render, screen } from '@testing-library/react';
-import { ThemeProvider } from './components/contexts/theme'
-import { to12Format, addLeadingZero } from './components/helper/formatTime'
-
+import React from 'react';
 import App from './App';
-describe('Render Clock', () => {
-  let date = new Date();
+import { ThemeProvider } from './components/contexts/theme';
+import { addLeadingZero, to12Format } from './components/helper/formatTime';
 
+describe('Testing App', () => {
   beforeEach(() => {
     render(<ThemeProvider><App /></ThemeProvider>);
   })
-  test('renders Hour:Minute in 12 format', () => {
-    const currentTime = `${date.getHours()}:${date.getMinutes()}`;
-    // const clock = getByText(to12Format(currentTime));
-    expect(screen.getByText(to12Format(currentTime))).toBeInTheDocument();
-  });
-  it('Renders Second', () => {
+
+  describe('Render Clock', () => {
     let date = new Date();
-    // const seconds = getByText(`:${addLeadingZero(date.getSeconds())}`)
-    expect(screen.getByText(`:${addLeadingZero(date.getSeconds())}`)).toBeInTheDocument();
+    test('renders Hour:Minute in 12 format', () => {
+      const currentTime = `${date.getHours()}:${date.getMinutes()}`;
+      // const clock = getByText(to12Format(currentTime));
+      expect(screen.getByText(to12Format(currentTime))).toBeInTheDocument();
+    });
+    it('Renders Second', () => {
+      let date = new Date();
+      // const seconds = getByText(`:${addLeadingZero(date.getSeconds())}`)
+      expect(screen.getByText(`:${addLeadingZero(date.getSeconds())}`)).toBeInTheDocument();
+    })
+    it("render AM or PM", () => {
+      // const dayOrNight = getByText(date.getHours() < 12 ? "AM" : "PM")
+      expect(screen.getByText(date.getHours() < 12 ? "AM" : "PM")).toBeInTheDocument();
+    })
   })
-  it("render AM or PM", () => {
-    // const dayOrNight = getByText(date.getHours() < 12 ? "AM" : "PM")
-    expect(screen.getByText(date.getHours() < 12 ? "AM" : "PM")).toBeInTheDocument();
-  })
-})
 
-describe('Renders the Footer', () => {
-  beforeEach(() => {
-    render(<ThemeProvider><App /></ThemeProvider>);
-  })
-  it('Render "⚙️"', () => {
-    expect(screen.getByText(/⚙️/i)).toBeInTheDocument()
+  describe('Renders the Footer', () => {
+    it('Render "⚙️"', () => {
+      expect(screen.getByText(/⚙️/i)).toBeInTheDocument()
+    })
   })
 
 })
