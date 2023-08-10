a2a46592481b7160db4072075446f14a594ac8b2
Mohamed Hafez <mohamedhafezmansour@gmail.com>
2023-08-10 16:38:27 +0300

    ✅ To handle negative numbers

--- src/components/helper/formatTime.js
+++ src/components/helper/formatTime.js
@@ -4,7 +4,8 @@
  * @param {string | number} number 
  * @returns {string} the number with leading zero
  */
-export const addLeadingZero = (number) => (number < 10 ? `0${number}` : number.toString());
+// export const addLeadingZero = (number) => (number < 10 ? `0${number}` : number.toString());
+export const addLeadingZero = (number) => number.toString().padStart(2, '0');
 
 
 /**
@@ -14,9 +15,9 @@ export const addLeadingZero = (number) => (number < 10 ? `0${number}` : number.t
  * @returns {string} time 05:39
  */
 export const to12Format = (time) => {
-  
-  let hour = parseInt(time.slice(0,time.indexOf(":")));
-  let minute = time.slice(time.indexOf(":")+1);
+
+  let hour = parseInt(time.slice(0, time.indexOf(":")));
+  let minute = time.slice(time.indexOf(":") + 1);
 
   //convert to 12 Formate
   hour = (hour % 12) || 12;
@@ -32,11 +33,11 @@ export const to12Format = (time) => {
 
 
 // Format time ( for nextPray )
-export const getTimeFormat = (num) =>{
+export const getTimeFormat = (num) => {
   num = Math.ceil(Math.abs(num) / 1000);
   const h = Math.floor(num / 3600);
-  num  %= 3600;
+  num %= 3600;
   const m = Math.floor(num / 60);
-  const s =  num % 60;
+  const s = num % 60;
   return (`${addLeadingZero(h)}:${addLeadingZero(m)}:${addLeadingZero(s)}`)
 }
