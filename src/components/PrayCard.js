a0f4d2070e1b69484a51743ddce2a628026312f6
Mohamed Hafez <mohamedhafezmansour@gmail.com>
2023-08-10 17:35:51 +0300

    Show prayer time in 12 format

--- src/components/PrayCard.js
+++ src/components/PrayCard.js
@@ -1,11 +1,11 @@
 import React from 'react'
-
-export default function PrayCard({name, time, active}) {
+import { to12Format } from './helper/formatTime'
+export default function PrayCard({ name, time, active }) {
 
     return (
         <li className={active}>
             <h3>{name}</h3>
-            <h3>{time}</h3>
+            <h3>{to12Format(time)}</h3>
         </li>
     )
 }
