4a6eb0b574665ab0bd9634f42a8f531f48880d82
Mohamed Hafez <mohamedhafezmansour@gmail.com>
2023-08-12 20:58:02 +0300

    ✅ Load page with theme stored in the local storage

---
+++ src/components/contexts/__test__/theme.test.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import { render, act } from '@testing-library/react';
+import { ThemeProvider, ThemeContext } from '../theme';
+
+// Mocking localStorage
+const mockSetItem = jest.fn();
+const mockGetItem = jest.fn();
+global.localStorage.__proto__.setItem = mockSetItem;
+global.localStorage.__proto__.getItem = mockGetItem;
+
+describe('ThemeProvider', () => {
+
+  it('should set initial theme from localStorage', () => {
+    mockGetItem.mockReturnValueOnce('true');
+
+    let contextValue;
+    render(
+      <ThemeProvider>
+        <ThemeContext.Consumer>
+          {value => {
+            contextValue = value;
+          }}
+        </ThemeContext.Consumer>
+      </ThemeProvider>
+    );
+
+    expect(contextValue[0].isDark).toBe(true);
+  });
+
+  it('should toggle theme', () => {
+    let contextValue;
+    render(
+      <ThemeProvider>
+        <ThemeContext.Consumer>
+          {value => {
+            contextValue = value;
+          }}
+        </ThemeContext.Consumer>
+      </ThemeProvider>
+    );
+
+    act(() => {
+      contextValue[1](); // call toggleTheme
+    });
+
+    expect(mockSetItem).toHaveBeenCalledWith('isDark', 'true');
+    expect(contextValue[0].isDark).toBe(true);
+  });
+});
