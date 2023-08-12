15d8c76470fb2950bbc5cb883eecddca2283173a
Mohamed Hafez <mohamedhafezmansour@gmail.com>
2023-08-12 14:49:44 +0300

    ✅ Renders Clock (Dark/Light) (English/Arabic)

---
+++ src/components/__tests__/Clock.test.js
@@ -0,0 +1,128 @@
+import { render, screen } from "@testing-library/react"
+import React from "react"
+import Clock from "../Clock"
+import { addLeadingZero, to12Format } from "../helper/formatTime"
+import { ThemeContext } from "../contexts/theme"
+import { LanguageContext } from "../contexts/languageContext"
+
+describe("Renders Clock (Dark/Light) (English/Arabic)", () => {
+    describe("Renders Clock in Dark Mode", () => {
+        beforeEach(() => {
+            render(
+                <ThemeContext.Provider value={[{ isDark: true }]}>
+                    <LanguageContext.Provider value={["en"]}>
+                        <Clock />
+                    </LanguageContext.Provider>
+                </ThemeContext.Provider>
+            )
+        })
+        describe("Clock", () => {
+            let date = new Date()
+            it("renders Hour:Minute in 12 format", () => {
+                const currentTime = `${date.getHours()}:${date.getMinutes()}`
+                expect(screen.getByText(to12Format(currentTime))).toBeInTheDocument()
+            })
+            it("Renders Second", () => {
+                let date = new Date()
+                expect(
+                    screen.getByText(`:${addLeadingZero(date.getSeconds())}`)
+                ).toBeInTheDocument()
+            })
+            it("render AM or PM", () => {
+                expect(
+                    screen.getByText(date.getHours() < 12 ? "AM" : "PM")
+                ).toBeInTheDocument()
+            })
+        })
+        describe("Enlish", () => {
+            it("Render Clock in Enlish", () => {
+                render(
+                    <ThemeContext.Provider value={[{ isDark: true }]}>
+                        <LanguageContext.Provider value={["ar"]}>
+                            <Clock />
+                        </LanguageContext.Provider>
+                    </ThemeContext.Provider>
+                )
+                let date = new Date()
+                expect(
+                    screen.getByText(date.getHours() < 12 ? "AM" : "PM")
+                ).toBeInTheDocument()
+            })
+        })
+        describe("Arabic", () => {
+            it("Render Clock in Arabic", () => {
+                render(
+                    <ThemeContext.Provider value={[{ isDark: true }]}>
+                        <LanguageContext.Provider value={["ar"]}>
+                            <Clock />
+                        </LanguageContext.Provider>
+                    </ThemeContext.Provider>
+                )
+                let date = new Date()
+                expect(
+                    screen.getByText(date.getHours() < 12 ? "صباحاً" : "مساءً")
+                ).toBeInTheDocument()
+            })
+        })
+    })
+
+    describe("Renders Clock in Light Mode", () => {
+        beforeEach(() => {
+            render(
+                <ThemeContext.Provider value={[{ isDark: false }]}>
+                    <LanguageContext.Provider value={["en"]}>
+                        <Clock />
+                    </LanguageContext.Provider>
+                </ThemeContext.Provider>
+            )
+        })
+        describe("Clock", () => {
+            let date = new Date()
+            it("renders Hour:Minute in 12 format", () => {
+                const currentTime = `${date.getHours()}:${date.getMinutes()}`
+                expect(screen.getByText(to12Format(currentTime))).toBeInTheDocument()
+            })
+            it("Renders Second", () => {
+                let date = new Date()
+                expect(
+                    screen.getByText(`:${addLeadingZero(date.getSeconds())}`)
+                ).toBeInTheDocument()
+            })
+            it("render AM or PM", () => {
+                expect(
+                    screen.getByText(date.getHours() < 12 ? "AM" : "PM")
+                ).toBeInTheDocument()
+            })
+        })
+        describe("Enlish", () => {
+            it("Render Clock in Enlish", () => {
+                render(
+                    <ThemeContext.Provider value={[{ isDark: true }]}>
+                        <LanguageContext.Provider value={["ar"]}>
+                            <Clock />
+                        </LanguageContext.Provider>
+                    </ThemeContext.Provider>
+                )
+                let date = new Date()
+                expect(
+                    screen.getByText(date.getHours() < 12 ? "AM" : "PM")
+                ).toBeInTheDocument()
+            })
+        })
+        describe("Arabic", () => {
+            it("Render Clock in Arabic", () => {
+                render(
+                    <ThemeContext.Provider value={[{ isDark: true }]}>
+                        <LanguageContext.Provider value={["ar"]}>
+                            <Clock />
+                        </LanguageContext.Provider>
+                    </ThemeContext.Provider>
+                )
+                let date = new Date()
+                expect(
+                    screen.getByText(date.getHours() < 12 ? "صباحاً" : "مساءً")
+                ).toBeInTheDocument()
+            })
+        })
+    })
+})
