7a828e74993a27e3760c3c458f840fcdecd975e0
Mohamed Hafez <mohamedhafezmansour@gmail.com>
2023-08-12 13:45:46 +0300

    ✅ Unit test for useCurrentDate

---
+++ src/components/hooks/__tests__/useCurrentDate.test.js
@@ -0,0 +1,23 @@
+import { renderHook } from '@testing-library/react-hooks';
+import { useCurrentDate } from '../useCurrentDate';
+
+jest.useFakeTimers();
+
+describe('Unit test for useCurrentDate', () => {
+    it('should return the current date', () => {
+        const { result } = renderHook(() => useCurrentDate());
+
+        expect(result.current).toBeInstanceOf(Date);
+    });
+
+    it('should update the date every second', () => {
+        const { result } = renderHook(() => useCurrentDate());
+
+        const initialDate = result.current;
+
+        // Fast-forward 1 second
+        jest.advanceTimersByTime(1000);
+
+        expect(result.current).not.toEqual(initialDate);
+    });
+});
