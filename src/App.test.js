5d311860a7ce5699b251165c1e3302413c8f60a6
Mohamed Hafez <mohamedhafezmansour@gmail.com>
2023-08-13 14:13:57 +0300

    🗑️ Remove duplicated test

--- src/App.test.js
+++ src/App.test.js
@@ -5,26 +5,6 @@ import { ThemeProvider } from './components/contexts/theme';
 import { addLeadingZero, to12Format } from './components/helper/formatTime';
 
 describe('Testing App', () => {
-  beforeEach(() => {
-    render(<ThemeProvider><App /></ThemeProvider>);
-  })
 
-  describe('Render Clock', () => {
-    let date = new Date();
-    test('renders Hour:Minute in 12 format', () => {
-      const currentTime = `${date.getHours()}:${date.getMinutes()}`;
-      // const clock = getByText(to12Format(currentTime));
-      expect(screen.getByText(to12Format(currentTime))).toBeInTheDocument();
-    });
-    it('Renders Second', () => {
-      let date = new Date();
-      // const seconds = getByText(`:${addLeadingZero(date.getSeconds())}`)
-      expect(screen.getByText(`:${addLeadingZero(date.getSeconds())}`)).toBeInTheDocument();
-    })
-    it("render AM or PM", () => {
-      // const dayOrNight = getByText(date.getHours() < 12 ? "AM" : "PM")
-      expect(screen.getByText(date.getHours() < 12 ? "AM" : "PM")).toBeInTheDocument();
-    })
-  })
 
 })
