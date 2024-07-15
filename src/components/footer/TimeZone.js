9a8316c0fae53d4891aa1d13e45d598b2af0b7ea
Mohamed Hafez <mohamedhafezmansour@gmail.com>
2024-07-15 15:42:35 +0300

    feat: Show method next to the location

--- src/components/footer/TimeZone.js
+++ src/components/footer/TimeZone.js
@@ -1,10 +1,10 @@
-import React, {useContext} from 'react'
-import {FetchedDataContext} from '../contexts/FetchedDataContext'
+import React, { useContext } from 'react'
+import { FetchedDataContext } from '../contexts/FetchedDataContext'
 
 function TimeZone() {
   const [loaded, data] = useContext(FetchedDataContext);
 
-  return  <h4>{loaded && data.meta.timezone}</h4>
+  return <h4>{loaded && data.meta.timezone} - {loaded && data.meta.method.name}</h4>
 }
 
 export default TimeZone;
