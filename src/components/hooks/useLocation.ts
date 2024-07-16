6151cedc8595a0e43c5d734f73f12b80ac4fe652
Mohamed Hafez <mohamedhafezmansour@gmail.com>
2024-07-17 00:36:02 +0300

    Add types to RemainTime

--- src/components/hooks/useLocation.ts
+++ src/components/hooks/useLocation.ts
@@ -7,22 +7,20 @@ import { useState } from 'react'
  */
 
 export const useLocation = () => {
-
     const [latitude, setLatitude] = useState("30.008");
     const [longitude, setLongitude] = useState("31.2194");
 
     // get current location 
-    const success = (position) => {
-        setLatitude(position.coords.latitude);
-        setLongitude(position.coords.longitude);
+    const success = (position: GeolocationPosition) => {
+        setLatitude(position.coords.latitude.toString());
+        setLongitude(position.coords.longitude.toString());
     }
 
-    const error = (position) => { console.log(position.message); }
+    const error = (error: GeolocationPositionError) => { console.log(error.message); }
 
     !navigator.geolocation ?
         console.log('Geolocation is not supported by your browser') :
         navigator.geolocation.getCurrentPosition(success, error);
 
     return [latitude, longitude]
-
 }
