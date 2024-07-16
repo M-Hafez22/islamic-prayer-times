7b6acf6d43a63a570efef950aef12058365b0c64
Mohamed Hafez <mohamedhafezmansour@gmail.com>
2024-07-16 23:57:08 +0300

    Added type annotation for PrayCard

--- src/components/PrayCard.tsx
+++ src/components/PrayCard.tsx
@@ -1,11 +1,15 @@
-import React from 'react'
-import { to12Format } from './helper/formatTime'
-export default function PrayCard({ name, time, active }) {
+import { to12Format } from "./helper/formatTime"
 
-    return (
-        <li className={active}>
-            <h3>{name}</h3>
-            <h3>{to12Format(time)}</h3>
-        </li>
-    )
+interface PrayCardProps {
+  name: string
+  time: string
+  active: string
+}
+export default function PrayCard({ name, time, active }: PrayCardProps) {
+  return (
+    <li className={active}>
+      <h3>{name}</h3>
+      <h3>{to12Format(time)}</h3>
+    </li>
+  )
 }
