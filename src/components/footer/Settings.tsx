c24697266bae0eee523df12b196c7d9ffd9418b6
Mohamed Hafez <mohamedhafezmansour@gmail.com>
2024-07-21 12:33:56 +0300

    Add types to Settings component

--- src/components/footer/Settings.tsx
+++ src/components/footer/Settings.tsx
@@ -2,7 +2,7 @@ import LanguageToggle from './LanguageToggle'
 import TimeZone from './TimeZone'
 import ThemeToggle from './ThemeToggle';
 
-function Settings({ show }) {
+function Settings({ show }: { show: boolean }) {
     return (
         <div className={show ? "showSettings settings" : "hideSettings settings"} data-testid={'setting'}>
             <LanguageToggle />
